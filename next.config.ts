a0fe7700e2c86e9d85d3ee39ca9bd44a01995e5c
Tymur <tymur@MacBook-Air-Tymur.local>
2026-05-09 20:42:51 +0200

    Support Vercel build without basePath

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
+const isVercel = process.env.VERCEL === "1";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/ozbarbershop" : "",
+  basePath: (isProd && !isVercel) ? "/ozbarbershop" : "",
   images: {
     unoptimized: true,
   },
